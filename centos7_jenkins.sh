e7df6a27a15af3ece507e7576dcdcf1859e52fba
jarielpa <jlammassaari@gmail.com>
2016-06-20 14:21:43 +0300

    last steps

--- centos7_jenkins.sh
+++ centos7_jenkins.sh
@@ -1,2 +1,4 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins
+sudo service jenkins start
