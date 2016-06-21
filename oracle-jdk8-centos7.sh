864f06c9797a3bcedea6088dc0aed419a2c5529c
jarielpa <jlammassaari@gmail.com>
2016-06-21 09:44:11 +0300

    oracle jkd 8

---
+++ oracle-jdk8-centos7.sh
@@ -0,0 +1,3 @@
+cd /opt/
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz"
+tar xzf jdk-8u91-linux-x64.tar.gz
