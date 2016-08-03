9c593a5b4be4accc2428ea579d76413febdbf943
ext-lammaja1 <ext-jari.lammassaari@posti.com>
2016-08-03 09:49:09 +0300

    update to 1.2.7

--- apiman.sh
+++ apiman.sh
@@ -2,19 +2,19 @@
 #
 # Licence: MIT
 
-if [ -d "/opt/apiman-1.2.6.Final" ]; then
+if [ -d "/opt/apiman-1.2.7.Final" ]; then
 	echo "There's already an installation of Apiman in /opt"
 	rm -f apiman.sh
 	echo "Skipping..."
 	exit 0
 fi
 
-mkdir /opt/apiman-1.2.6.Final
-cd /opt/apiman-1.2.6.Final
+mkdir /opt/apiman-1.2.7.Final
+cd /opt/apiman-1.2.7.Final
 curl http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.zip -o wildfly-10.0.0.Final.zip
-curl http://downloads.jboss.org/apiman/1.2.6.Final/apiman-distro-wildfly10-1.2.6.Final-overlay.zip -o apiman-distro-wildfly10-1.2.6.Final-overlay.zip
+curl http://downloads.jboss.org/apiman/1.2.7.Final/apiman-distro-wildfly10-1.2.7.Final-overlay.zip -o apiman-distro-wildfly10-1.2.7.Final-overlay.zip
 unzip wildfly-10.0.0.Final.zip
-unzip -o apiman-distro-wildfly10-1.2.6.Final-overlay.zip -d wildfly-10.0.0.Final
+unzip -o apiman-distro-wildfly10-1.2.7.Final-overlay.zip -d wildfly-10.0.0.Final
 #cd wildfly-10.0.0.Final
 #./bin/standalone.sh -c standalone-apiman.xml
 
