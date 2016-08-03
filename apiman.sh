e8cdb3778e739bb09b77ff2e0d532379317c548e
jarielpa <jlammassaari@gmail.com>
2016-08-03 14:07:35 +0300

    default location changed to user home

--- apiman.sh
+++ apiman.sh
@@ -2,15 +2,15 @@
 #
 # Licence: MIT
 
-if [ -d "/opt/apiman-1.2.7.Final" ]; then
-	echo "There's already an installation of Apiman in /opt"
-	rm -f apiman.sh
+if [ -d "~/apiman-1.2.7.Final" ]; then
+	echo "There's already an installation of Apiman"
+	#rm -f apiman.sh
 	echo "Skipping..."
 	exit 0
 fi
 
-mkdir /opt/apiman-1.2.7.Final
-cd /opt/apiman-1.2.7.Final
+mkdir ~/apiman-1.2.7.Final
+cd ~/apiman-1.2.7.Final
 curl http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.zip -o wildfly-10.0.0.Final.zip
 curl http://downloads.jboss.org/apiman/1.2.7.Final/apiman-distro-wildfly10-1.2.7.Final-overlay.zip -o apiman-distro-wildfly10-1.2.7.Final-overlay.zip
 unzip wildfly-10.0.0.Final.zip
@@ -18,7 +18,6 @@ unzip -o apiman-distro-wildfly10-1.2.7.Final-overlay.zip -d wildfly-10.0.0.Final
 cd wildfly-10.0.0.Final
 #./bin/standalone.sh -c standalone-apiman.xml
 
- 
-#sudo chown -R root:root /usr/maven
+
 
 
